0fb299fa410173365ac1493b2a08a78b0caeb02c
msi1354 <86691276+msi1354@users.noreply.github.com>
2021-07-08 12:24:25 +1200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,17 @@
+let dice = 0
 input.onButtonPressed(Button.A, function () {
-basic.showArrow(ArrowNames.West)  
+    basic.showArrow(ArrowNames.West)
 })
 input.onButtonPressed(Button.B, function () {
-basic.showArrow(ArrowNames.East)  
+    basic.showArrow(ArrowNames.East)
+})
+input.onGesture(Gesture.Shake, function () {
+   dice = randint(1, 6) 
+   basic.showNumber(dice) 
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+      if (dice = 2,4,6) {
+    }
+    basic.pause(100)
+    basic.showString("Yay!")
 })
